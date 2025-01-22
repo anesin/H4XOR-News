13f33bb286d279f67f98d3937b884bac550926e9
이재영 <jaeyoung.yi@seculetter.com>
2025-01-22 15:20:51 +0900

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by 이재영 on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
